bbd41895c7f4adbacff2240767a06b83ad6ca8de
Zarni <zmaung580@gmail.com>
2024-03-24 13:20:03 +0630

    First Command

---
+++ Intro.js
@@ -0,0 +1,14 @@
+function sayHello() {
+  alert("This is testing for javascript");
+}
+
+function getSum(first, second) {
+  var sum = first + second;
+  alert("The sum of " + first + " and " + second + " is " + sum);
+}
+
+function total() {
+  let first = 5;
+  let second = 5;
+  getSum(first, second);
+}
